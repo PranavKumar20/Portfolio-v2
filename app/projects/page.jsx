79bab399a9ba776b19342aab4ab5bce65aeb5872
= <pranavkumar0155@gmail.com>
2024-07-14 19:53:35 +0530

    added project folder

---
+++ app/projects/page.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "@/components/GeneralComponents/Navbar";
+import Title from "@/components/GeneralComponents/Title";
+import Footer from "@/components/GeneralComponents/Footer";
+
+const index = ()=>{
+    return (
+        <div>
+            hiii from projects page
+        </div>
+    )
+}
